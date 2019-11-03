8f2001093864000a5eda8013082e421eaca544b8
Andrey <andrey@makonix.com>
2019-11-03 17:49:48 +0200

    Telefon Entity and Form

---
+++ src/Form/AddTelefonType.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Telefon;
+use App\Validator\TruckNumberPlate;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class AddTelefonType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('phonenumber', IntegerType::class,[
+                'constraints' =>[
+                    new NotBlank(),
+                ],
+                'data' => '+371000000'
+            ])
+            ->add('billance', TextType::class,[
+                'constraints' => [
+                    new NotBlank()
+                ]
+            ])
+            ->add('truck', TextType::class,[
+                'constraints' => [
+                    new NotBlank(),
+                    new TruckNumberPlate()
+                ]
+            ])
+            ->add('submit', SubmitType::class)
+            ->setMethod('POST')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+
+        ]);
+    }
+}
